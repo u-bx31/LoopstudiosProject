8ca5aad70ca7d33fefd0400456fcc8512d8690cf
LabchariOussama <oussama.bx31@gmail.com>
2022-11-12 16:58:50 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from './Component/Navbar'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
